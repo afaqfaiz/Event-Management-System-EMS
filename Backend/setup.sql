aae152ab4d772f56c87ddeada8788ff14a04fba1
shoaibasgharmayo <bscs22130@itu.edu.pk>
2024-10-31 21:23:57 +0500

    Events table

--- Backend/setup.sql
+++ Backend/setup.sql
@@ -29,34 +29,3 @@ CREATE TABLE Events (
     Event_Attenders INT,
     FOREIGN KEY (Hall_ID) REFERENCES Hall(Hall_ID)
 );
-CREATE TABLE Bookings (
-    Booking_ID INT PRIMARY KEY,
-    Event_ID INT,
-    Hall_ID INT,
-    Client_ID INT,
-    Booking_Date DATE,
-    Booking_StartDateTime DATETIME,
-    Booking_EndDateTime DATETIME,
-    Company_ID INT,
-    Total_Cost DECIMAL(10, 2),
-    FOREIGN KEY (Event_ID) REFERENCES Events(Event_ID),
-    FOREIGN KEY (Hall_ID) REFERENCES Hall(Hall_ID),
-    FOREIGN KEY (Company_ID) REFERENCES Company(Company_ID)
-);
-CREATE TABLE Clients (
-    Client_ID INT PRIMARY KEY,
-    Client_Name VARCHAR(100),
-    Client_ContactNumber VARCHAR(15),
-    Client_Email VARCHAR(100),
-    Client_Address VARCHAR(255)
-);
-CREATE TABLE Payment (
-    Payment_ID INT PRIMARY KEY,
-    Booking_ID INT,
-    Client_ID INT,
-    Amount DECIMAL(10, 2),
-    Payment_Date DATE,
-    Payment_Method VARCHAR(50),
-    FOREIGN KEY (Booking_ID) REFERENCES Bookings(Booking_ID),
-    FOREIGN KEY (Client_ID) REFERENCES Clients(Client_ID)
-);
